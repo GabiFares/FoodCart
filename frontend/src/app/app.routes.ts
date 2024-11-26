86c966fabc9d18cb5cdf9c209eac74a8d8eaa7e0
unknown <emilio.rodriguez@correo.ucu.edu.uy>
2024-11-26 16:31:50 -0300

    Grafica de prueba solo para la prueba je

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -17,6 +17,7 @@ import { PoliticasPrivacidadPage } from './paginas/politicas-privacidad/politica
 import { TerminosServicioPage } from './paginas/terminos-servicio/terminos-servicio.page'; // Importa la página de términos y condiciones
 import { EditarPerfilPage } from './paginas/editar-perfil/editar-perfil.page';
 import { ContactoPage } from './paginas/contacto/contacto.page'; // Importa la página de contacto
+import { GraficaPage } from './prueba/grafica/grafica.page';
 
 export const routes: Routes = [
   {
@@ -111,5 +112,10 @@ export const routes: Routes = [
     component: ContactoPage,
     title: 'Contacto', // Título de la página
     canActivate: [checkAdminGuard], // Guard para verificar si el usuario está autenticado
+  },
+  {
+    path: 'ejemplo', // Ruta para la página de contacto
+    component: GraficaPage,
+    title: 'Grafica', // Título de la página
   },
 ];
